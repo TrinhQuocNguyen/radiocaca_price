74728ac657e2e6fe522a65a23eff6768203a26ef
trinhnguyen <nguyenquoctrinhctt3@gmail.com>
2022-01-05 16:37:35 +0900

    add dragon fruit

--- layers.py
+++ layers.py
@@ -11,111 +11,94 @@ def convert(row):
     #print(row)
     return '<a href="{}">{}</a>'.format(row['link'],  row.name)
 
-class kiss_land():
+
+
+class Item():
     def __init__(self):
+        # api-endpoint
+        self.URL = ""
+        # defining a params dict for the parameters to be sent to the API
+        self.PARAMS = {'address':"delhi technological university"}
+
+    def refine_data(self, data):
+        refined_data = []
+        id = []
+        fixed_price = []
+        link = []
+        for i in data['list']:
+            id.append(i['id'] )
+            fixed_price.append(i['fixed_price'] )
+            link_text = "https://market.radiocaca.com/#/market-place/" + str(i['id'])
+            link.append(link_text)
+        refined_data.append(id)
+        refined_data.append(fixed_price)
+        refined_data.append(link)
+        
+        return refined_data
+
+    def get_price(self):
+        # sending get request and saving the response as response object
+        r = requests.get(url = self.URL, params = self.PARAMS)
+        
+        # extracting data in json format
+        data = r.json()
+        refined_data = self.refine_data(data)
+
+        return refined_data
+
+
+class Kiss_Land(Item):
+    def __init__(self):
+        super().__init__()
         # api-endpoint
         self.URL = "https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=single_price&order=asc&name=&saleType&category=20&tokenType&tokenId=-1"
-        # defining a params dict for the parameters to be sent to the API
-        self.PARAMS = {'address':"delhi technological university"}
 
-    def refine_data(self, data):
-        refined_data = []
-        id = []
-        fixed_price = []
-        link = []
-        for i in data['list']:
-            id.append(i['id'] )
-            fixed_price.append(i['fixed_price'] )
-            link_text = "https://market.radiocaca.com/#/market-place/" + str(i['id'])
-            link.append(link_text)
-        refined_data.append(id)
-        refined_data.append(fixed_price)
-        refined_data.append(link)
-        
-        return refined_data
-
-    def get_price(self):
-        # sending get request and saving the response as response object
-        r = requests.get(url = self.URL, params = self.PARAMS)
-        
-        # extracting data in json format
-        data = r.json()
-        refined_data = self.refine_data(data)
-
-        return refined_data
-
-class new_harvard_n_land():
+   
+class New_Harvard_N_Land(Item):
     def __init__(self):
+        super().__init__()
         # api-endpoint
         self.URL = "https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=single_price&order=asc&name=&saleType&category=34&tokenType&tokenId=-1"
-        # defining a params dict for the parameters to be sent to the API
-        self.PARAMS = {'address':"delhi technological university"}
 
-    def refine_data(self, data):
-        refined_data = []
-        id = []
-        fixed_price = []
-        link = []
-        for i in data['list']:
-            id.append(i['id'] )
-            fixed_price.append(i['fixed_price'] )
-            link_text = "https://market.radiocaca.com/#/market-place/" + str(i['id'])
-            link.append(link_text)
-        refined_data.append(id)
-        refined_data.append(fixed_price)
-        refined_data.append(link)
-        
-        return refined_data
 
-    def get_price(self):
-        # sending get request and saving the response as response object
-        r = requests.get(url = self.URL, params = self.PARAMS)
-        
-        # extracting data in json format
-        data = r.json()
-        refined_data = self.refine_data(data)
-
-        return refined_data
-
-class musk_land():
+class Musk_Land(Item):
     def __init__(self):
+        super().__init__()
         # api-endpoint
         self.URL = "https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=single_price&order=asc&name=&saleType&category=7&tokenType&tokenId=-1"
-        # defining a params dict for the parameters to be sent to the API
-        self.PARAMS = {'address':"delhi technological university"}
 
-    def refine_data(self, data):
-        refined_data = []
-        id = []
-        fixed_price = []
-        link = []
-        for i in data['list']:
-            id.append(i['id'] )
-            fixed_price.append(i['fixed_price'] )
-            link_text = "https://market.radiocaca.com/#/market-place/" + str(i['id'])
-            link.append(link_text)
-        refined_data.append(id)
-        refined_data.append(fixed_price)
-        refined_data.append(link)
-        
-        return refined_data
-
-    def get_price(self):
-        # sending get request and saving the response as response object
-        r = requests.get(url = self.URL, params = self.PARAMS)
-        # extracting data in json format
-        data = r.json()
-        refined_data = self.refine_data(data)
-
-        return refined_data
-
-
-class metamon():
+class Musk_Land(Item):
     def __init__(self):
+        super().__init__()
+        # api-endpoint
+        self.URL = "https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=single_price&order=asc&name=&saleType&category=7&tokenType&tokenId=-1"
+
+class Egg(Item):
+    def __init__(self):
+        super().__init__()
+        # api-endpoint
+        self.URL = "https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=fixed_price&order=asc&name=&saleType&category=17&tokenType&tokenId=-1"
+
+
+class Potion(Item):
+    def __init__(self):
+        super().__init__()
+        # api-endpoint
+        self.URL = "https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=fixed_price&order=asc&name=&saleType&category=15&tokenType&tokenId=-1"
+
+
+class Dragon_Fruit_Dog(Item):
+    def __init__(self):
+        super().__init__()
+        # api-endpoint
+        self.URL = "https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=fixed_price&order=asc&name=&saleType&category=26&tokenType&tokenId=-1"
+
+
+class Metamon(Item):
+    def __init__(self):
+        super().__init__()
         # api-endpoint
         self.URL = "https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=single_price&order=asc&name=&saleType&category=13&tokenType&tokenId=-1"
-        # defining a params dict for the parameters to be sent to the API
-        self.PARAMS = {'address':"delhi technological university"}
 
     def refine_data(self, data):
         refined_data = []
@@ -142,13 +125,3 @@ class metamon():
         refined_data.append(score)
         
         return refined_data
-
-    def get_price(self):
-        # sending get request and saving the response as response object
-        r = requests.get(url = self.URL, params = self.PARAMS)
-        
-        # extracting data in json format
-        data = r.json()
-        refined_data = self.refine_data(data)
-
-        return refined_data        
--- streamlit_app.py
+++ streamlit_app.py
@@ -20,26 +20,37 @@ def convert_musk(row):
 def convert_metamon(row):
     # print(row)
     return '<a href="{}">{}</a>'.format(row['link_metamon'], "Link")
+def convert_egg(row):
+    # print(row)
+    return '<a href="{}">{}</a>'.format(row['link_egg'], "Link")
+def convert_potion(row):
+    # print(row)
+    return '<a href="{}">{}</a>'.format(row['link_potion'], "Link")
 
+def convert_dragon(row):
+    # print(row)
+    return '<a href="{}">{}</a>'.format(row['link_dragon'], "Link")
 
 # update every minute (60*1000)
-st_autorefresh(interval= 60 * 1000, key="dataframerefresh")
+st_autorefresh(interval= 5* 60 * 1000, key="dataframerefresh")
+# add_selectbox = st.sidebar.selectbox(
+#     "How would you like to be contacted?",
+#     ("Email", "Home phone", "Mobile phone")
+# )
 
-def get_full_price():
+def get_main():
     now = datetime.now()
     st.subheader('Last Update: '+ now.strftime("%d/%m/%Y %H:%M:%S"))
-
-
-    metamon = layers.metamon()
+    metamon = layers.Metamon()
     df_metamon = metamon.get_price()
 
-    kiss = layers.kiss_land()
+    kiss = layers.Kiss_Land()
     df_kiss = kiss.get_price()
 
-    harvard_n = layers.new_harvard_n_land()
+    harvard_n = layers.New_Harvard_N_Land()
     df_harvard_n = harvard_n.get_price()
 
-    musk_usm = layers.musk_land()
+    musk_usm = layers.Musk_Land()
     df_musk_usm = musk_usm.get_price()
 
     df_full = pd.DataFrame({
@@ -64,12 +75,42 @@ def get_full_price():
     df_full['link_musk'] = df_full.apply(convert_musk, axis=1)
     df_full['link_metamon'] = df_full.apply(convert_metamon, axis=1)
 
-    # df_kiss = get_kiss_land_price()
-    # st.write(df_kiss.to_html(escape=False), unsafe_allow_html=True)
-    
-    # df_metamon = get_kiss_land_price()
     st.write(df_full.to_html(escape=False), unsafe_allow_html=True)
-    # st.dataframe(df_full.style.highlight_min(axis=0))
+
+
+def get_egg():
+    now = datetime.now()
+    st.subheader('Last Update: '+ now.strftime("%d/%m/%Y %H:%M:%S"))
+    egg = layers.Egg()
+    df_egg = egg.get_price()
+
+    potion = layers.Potion()
+    df_potion = potion.get_price()
+
+    dragon = layers.Dragon_Fruit_Dog()
+    df_dragon = dragon.get_price()
+
+    df_full = pd.DataFrame({
+        'EGG' : df_egg[1],
+        'link_egg' : df_egg[2],
+
+        'POTION' : df_potion[1],
+        'link_potion' : df_potion[2],
+
+        'DRAGON_FRUIT' : df_dragon[1],
+        'link_dragon' : df_dragon[2],
+    })
+
+    df_full['link_egg'] = df_full.apply(convert_egg, axis=1)
+    df_full['link_potion'] = df_full.apply(convert_potion, axis=1)
+    df_full['link_dragon'] = df_full.apply(convert_dragon, axis=1)
+
+    st.write(df_full.to_html(escape=False), unsafe_allow_html=True)
+   
+def get_full_price():
+    get_main()
+    get_egg()
+
     
 
 st.dataframe(get_full_price())
